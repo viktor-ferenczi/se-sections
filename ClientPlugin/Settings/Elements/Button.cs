e75cbf801021cc03e732572573348bf8a2562745
Viktor Ferenczi <viktor@ferenczi.eu>
2024-12-16 10:57:22 +0100

    Initial commit

---
+++ ClientPlugin/Settings/Elements/Button.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox.Graphics.GUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientPlugin.Settings.Elements
+{
+    internal class ButtonAttribute : Attribute, IElement
+    {
+        public readonly string Label;
+        public readonly string Description;
+
+        public ButtonAttribute(string label = null, string description = null)
+        {
+            
+            Label = label;
+            Description = description;
+        }
+
+        public List<MyGuiControlBase> GetElements(string name, Func<object> propertyGetter, Action<object> propertySetter)
+        {
+            var label = Tools.GetLabelOrDefault(name, Label);
+            var button = new MyGuiControlButton(text: new StringBuilder(label), toolTip: Description);
+            button.ButtonClicked += (_)=>((Action)propertyGetter())();
+
+            return new List<MyGuiControlBase>() { button };
+        }
+        public List<Type> SupportedTypes { get; } = new List<Type>()
+        {
+            typeof(Delegate)
+        };
+    }
+}
---
+++ ClientPlugin/Settings/Elements/Textbox.cs
@@ -0,0 +1,36 @@
+﻿using Sandbox.Graphics.GUI;
+using System;
+using System.Collections.Generic;
+
+namespace ClientPlugin.Settings.Elements
+{
+    internal class TextboxAttribute : Attribute, IElement
+    {
+        public readonly string Label;
+        public readonly string Description;
+
+        public TextboxAttribute(string label = null, string description = null)
+        {
+            Label = label;
+            Description = description;
+        }
+
+        public List<MyGuiControlBase> GetElements(string name, Func<object> propertyGetter, Action<object> propertySetter)
+        {
+            var textBox = new MyGuiControlTextbox(defaultText: (string)propertyGetter());
+            textBox.TextChanged += (box)=>propertySetter(box.Text);
+            textBox.SetToolTip(Description);
+
+            var label = Tools.GetLabelOrDefault(name, Label);
+            return new List<MyGuiControlBase>()
+            {
+                new MyGuiControlLabel(text: label),
+                textBox,
+            };
+        }
+        public List<Type> SupportedTypes { get; } = new List<Type>()
+        {
+            typeof(string)
+        };
+    }
+}
