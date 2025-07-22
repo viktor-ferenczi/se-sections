bac6fbf09403503095c58a6e166373b11759e2c3
Viktor Ferenczi <viktor@ferenczi.eu>
2025-07-22 02:24:45 +0200

    Cleanup

--- ClientPlugin/Logic/DataStorage.cs
+++ ClientPlugin/Logic/DataStorage.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using Sandbox.Game.Entities.Cube;
--- ClientPlugin/Logic/References.cs
+++ ClientPlugin/Logic/References.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sandbox.Game.Entities;
 using Sandbox.Game.Entities.Cube;
-using Sandbox.Game.Gui;
 
 namespace ClientPlugin.Logic
 {
